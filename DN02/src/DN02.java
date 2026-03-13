dc37b30e0fb17cfc757b8dd81a33d0aa4cdfc62a
Simon <simonblazic1@gmail.com>
2026-03-13 17:13:33 +0100

    Fixed space offset problem

--- DN02/src/DN02.java
+++ DN02/src/DN02.java
@@ -6,10 +6,12 @@ public class DN02 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 int charPointer = n * i + j;
+
+                System.out.print(" ");
                 if (charPointer >= besedilo.length()) {
-                    System.out.print(".  ");
+                    System.out.print(". ");
                 } else {
-                    System.out.print(besedilo.charAt(charPointer) + "  ");
+                    System.out.print(besedilo.charAt(charPointer) + " ");
                 }
             }
             System.out.println();
